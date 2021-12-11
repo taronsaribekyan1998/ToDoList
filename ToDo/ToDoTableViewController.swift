c2676a37508e6156d74c2b07834d0f2f1d1af81c
Taron Saribekyan <saribekyantaron1998@gmail.com>
2021-12-11 11:45:48 +0400

    part 3 code added

--- ToDo/ToDoTableViewController.swift
+++ ToDo/ToDoTableViewController.swift
@@ -13,15 +13,17 @@ class ToDoTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+        
         if let savedToDos = ToDo.loadToDos() {
             todos = savedToDos
-            } else {
-                todos =  ToDo.loadSampleToDos()
-            }
+        } else {
+            todos =  ToDo.loadSampleToDos()
+        }
     }
     
-    // MARK: - Table view data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todos.count
     }
@@ -32,5 +34,19 @@ class ToDoTableViewController: UITableViewController {
         let todo = todos[indexPath.row]
         cell.textLabel?.text = todo.title
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt
+                            indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit
+                            editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath:
+                            IndexPath) {
+        if editingStyle == .delete {
+            todos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
 }
